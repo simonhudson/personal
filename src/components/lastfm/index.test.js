44df4a50a11effb2b3e6a9ab231b8968deb40177
Simon Hudson <hellosimonhudson [at] gmail.com>
2023-05-12 15:07:27 +0100

    lastfm tests

---
+++ src/components/lastfm/index.test.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import LastFm from './index';
+import LastFmData from 'test/data/lastfm';
+import { getByTestId, render } from 'test/utils';
+import { act } from 'react-test-renderer';
+
+const ORIGINAL_FETCH = global.fetch;
+const ORIGINAL_LASTFM_USERNAME = process.env.LASTFM_USERNAME;
+const ORIGINAL_LASTFM_API_KEY = process.env.LASTFM_API_KEY;
+
+describe('LastFm', () => {
+	beforeEach(() => {
+		// global.fetch = jest.fn(() =>
+		// 	Promise.resolve({
+		// 		json: () => Promise.resolve(LastFmData),
+		// 	})
+		// );
+		global.fetch = jest.fn();
+		process.env.LASTFM_USERNAME = 'LASTFM_USERNAME';
+		process.env.LASTFM_API_KEY = 'LASTFM_API_KEY';
+	});
+
+	afterEach(() => {
+		fetch.mockClear();
+		global.fetch = ORIGINAL_FETCH;
+		process.env.LASTFM_USERNAME = ORIGINAL_LASTFM_USERNAME;
+		process.env.LASTFM_API_KEY = ORIGINAL_LASTFM_API_KEY;
+	});
+
+	const assertError = () => {
+		expect(getByTestId('lastfm__error')).toBeInTheDocument();
+		expect(getByTestId('lastfm__error').textContent).toEqual(`Sorry, couldn't load data from Last.fm :o(`);
+	};
+
+	it('should render basic UI', async () => {
+		// When
+		await initialise();
+
+		// Then
+		expect(getByTestId('lastfm__heading').textContent).toEqual('Now playing');
+	});
+
+	describe('Fetching data', () => {
+		it('should make call with expected arguments', async () => {
+			// When
+			await initialise();
+
+			// Then
+			expect(global.fetch).toHaveBeenCalledTimes(1);
+			expect(global.fetch).toHaveBeenCalledWith(
+				`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=LASTFM_USERNAME&api_key=LASTFM_API_KEY&format=json&limit=1`
+			);
+		});
+
+		describe('Error handling', () => {
+			it('should handle error response', async () => {
+				// Given
+				global.fetch = jest.fn(() =>
+					Promise.resolve({
+						status: 400,
+					})
+				);
+
+				// When
+				await initialise();
+
+				// Then
+				assertError();
+			});
+
+			it('should handle absence of expected data', async () => {
+				// Given
+				global.fetch = jest.fn(() =>
+					Promise.resolve({
+						json: () => Promise.resolve('foo'),
+					})
+				);
+
+				// When
+				await initialise();
+
+				// Then
+				assertError();
+			});
+		});
+		describe('Success handling', () => {
+			it(`should render data`, async () => {
+				// Given
+				global.fetch = jest.fn(() =>
+					Promise.resolve({
+						json: () => Promise.resolve(LastFmData),
+						status: 200,
+					})
+				);
+
+				// When
+				await initialise();
+
+				// Then
+				expect(getByTestId('lastfm__image__link')).toHaveAttribute('href', '/track-url');
+				expect(getByTestId('lastfm__image__image')).toHaveAttribute(
+					'alt',
+					`Now playing "Track Name" by Artist on Last.fm`
+				);
+				expect(getByTestId('lastfm__image__image')).toHaveAttribute('src', `large.jpg`);
+			});
+		});
+	});
+
+	const initialise = async () => await act(async () => render(<LastFm />));
+});
--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -7,7 +7,7 @@ const Loading = ({ isLoading }) => {
 	if (!isLoading) return null;
 
 	return (
-		<StyledLoading viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+		<StyledLoading viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" data-testid="loading">
 			<path d="M10 50A40 40 0 0 0 90 50A40 42 0 0 1 10 50" fill={`${palette.primary.bodyText}`} stroke="none">
 				<animateTransform
 					attributeName="transform"
