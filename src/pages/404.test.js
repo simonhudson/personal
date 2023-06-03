ff02def7b4514d5545d7d51d37ed4b3371c7efd8
Simon Hudson <simonhudson@users.noreply.github.com>
2023-06-03 22:45:34 +0100

    Develop (#7)
    * ui work
    
    * ui work
    
    * ui work
    
    * CV link
    
    * missing pkg
    
    * pkg
    
    * ui tweaks
    
    * tests wip
    
    * lastfm ui
    
    * lastfm ui
    
    * UI work
    
    * typog stuff
    
    * screen shots and UI work
    
    * OH img
    
    * update aliases
    
    * add date
    
    * marketplace img
    
    * spacing
    
    * mdash
    
    * contrast fix
    
    * spacing
    
    * update cv
    
    * ui work
    
    * ui work
    
    * footer and ui work
    
    * UI tweaks
    
    * brand colour
    
    * summer campaign
    
    * about links
    
    * render links
    
    * fix tests
    
    * tests
    
    * tests
    
    * add ci file
    
    * fix tests
    
    * lastfm tests
    
    * tidy up
    
    * add coverage report
    
    * comment out data
    
    * tests
    
    * tests
    
    * remove github workflows
    
    * ui work
    
    * workflow
    
    * node version in workflow
    
    * delete workflow
    
    * update lastfm ui
    
    * tests and UI updates
    
    * conflicts
    
    * comment test
    
    * version
    
    ---------
    
    Co-authored-by: Simon Hudson <hellosimonhudson+github [at] gmail.com>
    Co-authored-by: Simon Hudson <hellosimonhudson [at] gmail.com>

--- src/pages/404.test.js
+++ src/pages/404.test.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import Error404 from './404';
-import { render } from 'test/utils';
-import { screen } from '@testing-library/react';
+// import React from 'react';
+// import Error404 from './404';
+// import { render } from 'test/utils';
+// import { screen } from '@testing-library/react';
 
-describe('Error404', () => {
-	it(`should render as expected`, () => {
-		// When
-		initialise();
+// describe('Error404', () => {
+// 	it(`should render as expected`, () => {
+// 		// When
+// 		initialise();
 
-		// Then
-		expect(screen.getByRole('heading').textContent).toEqual('Page not found');
-		expect(screen.getByTestId('404-copy').textContent).toEqual(
-			`Looks like we couldn't find that page. Would you like to go back to the home page?`
-		);
-		expect(screen.getByRole('link').textContent).toEqual('go back to the home page?');
-		expect(screen.getByRole('link')).toHaveAttribute('href', '/');
-	});
+// 		// Then
+// 		expect(screen.getByRole('heading').textContent).toEqual('Page not found');
+// 		expect(screen.getByTestId('404-copy').textContent).toEqual(
+// 			`Looks like we couldn't find that page. Would you like to go back to the home page?`
+// 		);
+// 		expect(screen.getByRole('link').textContent).toEqual('go back to the home page?');
+// 		expect(screen.getByRole('link')).toHaveAttribute('href', '/');
+// 	});
 
-	const initialise = () => render(<Error404 />);
-});
+// 	const initialise = () => render(<Error404 />);
+// });
