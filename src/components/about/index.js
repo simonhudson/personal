fbac9bf7db373ecb2bbb45a675b23f114b856b08
Simon Hudson <simonhudson@users.noreply.github.com>
2023-07-01 08:55:46 +0100

    Update index.js

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -19,10 +19,16 @@ const AboutMe = () => {
 						</Strong>
 					</Paragraph>
 					<Paragraph>
-						I have been working in web design and development since 2004 and have a strong understanding of
-						WCAG/WAI requirements, progressive enhancement/graceful degradation, as well as experience
-						developing for assistive technologies. I am passionate about what I do and meticulous in all
-						stages of my work, from planning and coding, through to reviewing and bug fixing.
+						I've been working in web development since 2004, back when all this was just fields and we had to use <Link href="https://gist.github.com/Awilum/5589742"><a>all manner of nasty hacks to get our CSS working</a></Link>. In that time I have worked on a wide range of project, from small brochure-ware microsites through to enterprise-level single-page web apps serving millions of users.
+					</Paragraph>
+					<Paragraph>
+						I'm currently working at Experian, where I spend most of my day working with React and Node to build and maintain their Marketplace web app.
+					</Paragraph>
+					<Paragraph>
+						I have a real passion for building accessible user interfaces which can be easily used by as many people as possible. I enjoy trying to identify any barriers which people may face when using applications, and come up with creative solutions to make their experience as frictionless as possible.
+					</Paragraph>
+					<Paragraph>
+						From a code point-of-view, I'm a big fan of clean code, test-driven development, and semantic HTML.
 					</Paragraph>
 					{Links && (
 						<LinksList>
