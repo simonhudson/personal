17a70d8f349a3007d9486410656e8be6e270ad32
Simon Hudson <hellosimonhudson [at] gmail.com>
2023-07-18 21:18:20 +0100

    test coverage

--- src/components/logo/index.styles.tsx
+++ src/components/logo/index.styles.tsx
@@ -1,20 +1,10 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { rem } from 'polished';
 
 const StyledLogo = styled.svg`
 	height: auto;
 	margin: 0 0 ${({ theme }) => theme.spacing.default};
 	width: ${rem(50)};
-
-	${(props) =>
-		props.context === 'footer' &&
-		css`
-			display: block;
-			filter: grayscale(1);
-			opacity: 0.2;
-			margin: 0 auto;
-			width: ${rem(40)};
-		`};
 `;
 
 export { StyledLogo };
