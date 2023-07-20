a1324ddb08c32581b33e0d28ed93ebf809ba1c8c
Simon Hudson <simonhudson@users.noreply.github.com>
2023-07-20 13:33:09 +0100

    Develop (#28)
    * ui work
    
    * ui work
    
    * ui work
    
    * CV link
    
    * missing pkg
    
    * pkg
    
    * ui tweaks
    
    * tests wip
    
    * lastfm ui
    
    * lastfm ui
    
    * UI work
    
    * typog stuff
    
    * screen shots and UI work
    
    * OH img
    
    * update aliases
    
    * add date
    
    * marketplace img
    
    * spacing
    
    * mdash
    
    * contrast fix
    
    * spacing
    
    * update cv
    
    * ui work
    
    * ui work
    
    * footer and ui work
    
    * UI tweaks
    
    * brand colour
    
    * summer campaign
    
    * about links
    
    * render links
    
    * fix tests
    
    * tests
    
    * tests
    
    * add ci file
    
    * fix tests
    
    * lastfm tests
    
    * tidy up
    
    * add coverage report
    
    * comment out data
    
    * tests
    
    * tests
    
    * remove github workflows
    
    * ui work
    
    * workflow
    
    * node version in workflow
    
    * delete workflow
    
    * update lastfm ui
    
    * tests and UI updates
    
    * conflicts
    
    * comment test
    
    * version
    
    * build command
    
    * remove test
    
    * version
    
    * element ordering
    
    * tests and UI
    
    * UI fixes
    
    * spacing
    
    * fix key
    
    * Update index.js
    
    * Update index.js
    
    * Update global.styles.js
    
    * Update layout.js
    
    * Update index.styles.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * fix tests
    
    * tidy up media queries
    
    * font sizing
    
    * h3
    
    * Update index.js
    
    * Typescript (#19)
    
    * init commit
    
    * convert icon
    
    * fix console err
    
    * wip
    
    * big rework
    
    * rework
    
    * tests wip
    
    * wip
    
    * tests wip
    
    * test updates
    
    * wip
    
    * fix tests
    
    * tests
    
    * remove test
    
    ---------
    
    Co-authored-by: Simon Hudson <hellosimonhudson [at] gmail.com>
    
    * tidy up
    
    * update types
    
    * gitignore
    
    * version bump
    
    * fix tests
    
    * test coverage
    
    * update cv
    
    * version
    
    * jest config
    
    * ga tagging
    
    * fix GA
    
    ---------
    
    Co-authored-by: Simon Hudson <hellosimonhudson+github [at] gmail.com>
    Co-authored-by: Simon Hudson <hellosimonhudson [at] gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,13 +5,6 @@ import { ThemeProvider } from 'styled-components';
 import Theme from 'src/theme';
 import { GlobalStyles } from 'src/theme/global.styles';
 
-// Log accessibility issues to console in non-production environments
-if (process.env.APP_ENV !== 'production' && typeof window !== 'undefined') {
-	const ReactDOM = require('react-dom');
-	const axe = require('react-axe');
-	axe(React, ReactDOM, 1000);
-}
-
 const App = ({ Component, pageProps }) => {
 	pageProps.holdingMode = process?.env?.HOLDING_MODE?.toLowerCase() === 'true';
 
@@ -21,19 +14,15 @@ const App = ({ Component, pageProps }) => {
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<title>Simon Hudson | Front&ndash;end Developer</title>
 			</Head>
-			{process.env.APP_ENV === 'production' && typeof window !== 'undefined' && (
-				<>
-					<script async src="https://www.googletagmanager.com/gtag/js?id=G-BJLJ613G5T" />
-					<Script id="google-analytics">
-						{`
-							window.dataLayer = window.dataLayer || [];
-							function gtag(){dataLayer.push(arguments);}
-							gtag('js', new Date());
-							gtag('config', 'G-BJLJ613G5T');
-						`}
-					</Script>
-				</>
-			)}
+			<script async src="https://www.googletagmanager.com/gtag/js?id=G-BJLJ613G5T" />
+			<Script id="google-analytics">
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'G-BJLJ613G5T');
+				`}
+			</Script>
 			<ThemeProvider theme={Theme}>
 				<GlobalStyles />
 				<main>
