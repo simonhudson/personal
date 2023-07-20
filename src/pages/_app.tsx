c4c3f53824125ecf2d9d3e13b914f061b1046428
Simon Hudson <hellosimonhudson [at] gmail.com>
2023-07-20 13:59:17 +0100

    env logic for tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,21 +8,32 @@ import { GlobalStyles } from 'src/theme/global.styles';
 const App = ({ Component, pageProps }) => {
 	pageProps.holdingMode = process?.env?.HOLDING_MODE?.toLowerCase() === 'true';
 
+	// Log accessibility issues to console in non-production environments
+	if (process.env.APP_ENV !== 'production' && typeof window !== 'undefined') {
+		const ReactDOM = require('react-dom');
+		const axe = require('react-axe');
+		axe(React, ReactDOM, 1000);
+	}
+
 	return (
 		<>
 			<Head>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<title>Simon Hudson | Front&ndash;end Developer</title>
 			</Head>
-			<script async src="https://www.googletagmanager.com/gtag/js?id=G-BJLJ613G5T" />
-			<Script id="google-analytics">
-				{`
+			{process.env.APP_ENV === 'production' && (
+				<>
+					<script async src="https://www.googletagmanager.com/gtag/js?id=G-BJLJ613G5T" />
+					<Script id="google-analytics">
+						{`
 					window.dataLayer = window.dataLayer || [];
 					function gtag(){dataLayer.push(arguments);}
 					gtag('js', new Date());
 					gtag('config', 'G-BJLJ613G5T');
 				`}
-			</Script>
+					</Script>
+				</>
+			)}
 			<ThemeProvider theme={Theme}>
 				<GlobalStyles />
 				<main>
