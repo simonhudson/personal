2bf7977f6c29d957c6a0a02dd0ca8ad192e66eda
Simon Hudson <simonhudson@users.noreply.github.com>
2023-08-19 11:17:15 +0100

    Update palette.ts

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -25,7 +25,7 @@ type Palette = {
 const palette: Palette = {
 	primary: {
 		bodyBackground: '#fef9f5',
-		brand: '#fa5c35',
+		brand: '#C72C05',
 		bodyText: '#5d5d5d',
 		black: '#333',
 		white: '#fff',
