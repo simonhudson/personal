f053768dc01630d98b37eac4f0eec1326fba8e48
Simon Hudson <hellosimonhudson [at] gmail.com>
2023-09-07 16:04:17 +0100

    fix build

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { routes } from '@/src/constants/routes';
+import routes from '@/src/constants/routes';
 import Typography from '@/src/theme/typography';
 import Layout from '@/src/theme/layout';
 
