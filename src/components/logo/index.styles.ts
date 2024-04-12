f2976eb7e07ff051cfee6b69197c18b2ad565ac5
Simon Hudson <hellosimonhudson@gmail.com>
2024-04-12 15:16:43 +0100

    logo tweak

--- src/components/logo/index.styles.ts
+++ src/components/logo/index.styles.ts
@@ -5,5 +5,5 @@ import { spacingRem } from '@/src/theme/spacing';
 export const StyledLogo = styled.svg`
 	height: auto;
 	margin: 0 0 ${spacingRem.default};
-	width: ${rem(50)};
+	width: ${rem(75)};
 `;
