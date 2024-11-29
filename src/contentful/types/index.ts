49394dcd8321042d380dee7e41c563be05bd04bf
Simon Hudson <hellosimonhudson@gmail.com>
2024-11-29 09:26:20 +0000

    generate content types

--- src/contentful/types/index.ts
+++
@@ -1,3 +0,0 @@
-export type { TypeAboutMe, TypeAboutMeFields, TypeAboutMeSkeleton } from './TypeAboutMe';
-export type { TypeHero, TypeHeroFields, TypeHeroSkeleton } from './TypeHero';
-export type { TypePortfolioItem, TypePortfolioItemFields, TypePortfolioItemSkeleton } from './TypePortfolioItem';
