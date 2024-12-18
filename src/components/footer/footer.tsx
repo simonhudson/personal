e3f90c930821ca28082c138929a3f8fac0331701
Simon Hudson <hellosimonhudson@gmail.com>
2024-12-18 14:09:37 +0000

    footer ui

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,9 @@
 // import { LastFm } from '@/components/last-fm/last-fm';
+import styles from './footer.module.scss';
 
 export const Footer = () => {
 	return (
-		<footer>
+		<footer className={styles.footer}>
 			<div className="inner-wrap">
 				<p>Copyright &copy; Simon Hudson {new Date().getFullYear()}</p>
 				{/* <LastFm /> */}
