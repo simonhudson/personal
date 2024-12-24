5a5366d90805455bf76d9af91975a2ac5a847b49
Simon Hudson <hellosimonhudson@gmail.com>
2024-12-24 07:44:02 +0000

    update music widget

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,11 @@
-import { LastFm } from '@/components/last-fm/last-fm';
+import { Music } from '@/components/music/music';
 import styles from './footer.module.scss';
 
 export const Footer = () => {
 	return (
 		<footer className={styles.footer}>
 			<div className="inner-wrap">
-				<LastFm />
+				<Music />
 				{/* <p>Copyright &copy; Simon Hudson {new Date().getFullYear()}</p> */}
 			</div>
 		</footer>
