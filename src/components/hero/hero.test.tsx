83e0892a12538a062ddecef1b77bb1ccd1cb62eb
Simon Hudson <hellosimonhudson@gmail.com>
2025-01-06 20:55:52 +0000

    ui work

--- src/components/hero/hero.test.tsx
+++ src/components/hero/hero.test.tsx
@@ -1,20 +1,10 @@
 import { Hero } from './hero';
-import { HeroMock } from '@/test/mocks/cms/hero.mock';
 import { screen, render } from '@testing-library/react';
 
 describe('Hero', () => {
-	it('should render nothing if no data is provided', () => {
-		// When
-		render(<Hero />);
-
-		// Then
-		expect(screen.queryByRole('heading')).not.toBeInTheDocument();
-		expect(screen.queryByRole('paragraph')).not.toBeInTheDocument();
-	});
-
 	it(`should render as expected`, () => {
 		// When
-		render(<Hero data={HeroMock} />);
+		render(<Hero />);
 
 		// Then
 		expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(`Hello, my name's Simon Hudson`);
