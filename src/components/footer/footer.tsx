ab4dcd49a671fc4777c7e6533e863bb1aa603fdc
Simon Hudson <hellosimonhudson@gmail.com>
2025-01-17 13:39:03 +0000

    layout work

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,9 @@
-import { Music } from '@/src/components/music/music';
 import styles from './footer.module.scss';
 
 export const Footer = () => {
 	return (
 		<footer className={styles.footer}>
 			<div className="inner-wrap">
-				<Music />
 				<p>Copyright &copy; Simon Hudson {new Date().getFullYear()}</p>
 			</div>
 		</footer>
