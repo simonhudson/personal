46da1bcbb490568f6c4348ae21808c03659e44e6
Simon Hudson <simonhudson@users.noreply.github.com>
2025-04-23 13:41:31 +0100

    Dev (#96)
    * fix cta widths
    
    * lastfm wip
    
    * add lastfm
    
    * lastfm tweaks
    
    * update music widget
    
    * fix dynamic rendering
    
    * add absolute api url
    
    * test set up
    
    * add husky
    
    * fix types
    
    * test wip
    
    * fix test types
    
    * fix test types
    
    * add about tests
    
    * fix sass warnings
    
    * update icons
    
    * update icon props
    
    * revert icons
    
    * fix tests
    
    * add tests
    
    * add tests
    
    * update readme
    
    * update secrets
    
    * update workflow
    
    * update eslint
    
    * update eslint
    
    * update workflow
    
    * updater workflows
    
    * update workflow
    
    * update workflows
    
    * update workflows
    
    * ui work
    
    * fix test
    
    * layout work
    
    * cta styling
    
    * fix test
    
    * update rendering
    
    * remove lint override
    
    * music loading states
    
    * tidy up
    
    * ui work
    
    * update test

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,12 @@
 import styles from './footer.module.scss';
+import { Music } from '@/src/components/music/music';
 
 export const Footer = () => {
 	return (
 		<footer className={styles.footer}>
 			<div className="inner-wrap">
 				<p>Copyright &copy; Simon Hudson {new Date().getFullYear()}</p>
+				<Music />
 			</div>
 		</footer>
 	);
