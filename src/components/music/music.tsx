b383ecb4a62852c18a3b818d333f659c851bf23e
Simon Hudson <hellosimonhudson@gmail.com>
2025-04-23 13:16:31 +0100

    music loading states

--- src/components/music/music.tsx
+++ src/components/music/music.tsx
@@ -7,35 +7,43 @@ import { type MusicResponse } from '@/src/types/music';
 
 export const Music = () => {
 	const [data, setData] = useState<MusicResponse | null>(null);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 
 	useEffect(() => {
 		(async () => {
 			const response = await fetch(`${process.env.NEXT_PUBLIC_APP_DOMAIN}/api/music`);
 			const responseData: MusicResponse = await response.json();
 			setData(responseData);
+			setIsLoading(false);
 		})();
 	}, []);
 
-	return data ? (
+	return (
 		<div className={styles.wrap}>
 			<h3 className={styles.heading}>Now playing</h3>
 			<div className={styles.inner}>
-				<Image
-					alt={`Now playing "${data.name}" by ${data.artist}`}
-					className={styles.image}
-					height={300}
-					src={data.image}
-					width={300}
-				/>
-				<p className={styles.text}>
-					<span>
-						<Link href={data.url}>&quot;{data.name}&quot;</Link>{' '}
-					</span>
-					<span>
-						<Link href={data.url}> {data.artist}</Link>
-					</span>
-					<span>{data.relativeTime}</span>
-				</p>
+				{isLoading && <p>Loading&hellip;</p>}
+				{!isLoading && !data && <p>Sorry, we could not load data from Last.fm</p>}
+				{!isLoading && data && (
+					<>
+						<Image
+							alt={`Now playing "${data.name}" by ${data.artist}`}
+							className={styles.image}
+							height={300}
+							src={data.image}
+							width={300}
+						/>
+						<p className={styles.text}>
+							<span>
+								<Link href={data.url}>&quot;{data.name}&quot;</Link>{' '}
+							</span>
+							<span>
+								<Link href={data.url}> {data.artist}</Link>
+							</span>
+							<span>{data.relativeTime}</span>
+						</p>
+					</>
+				)}
 			</div>
 			<Image
 				alt=""
@@ -47,7 +55,7 @@ export const Music = () => {
 				width={64}
 			/>
 		</div>
-	) : (
-		<p>Sorry, we could not load data from Last.fm</p>
+		// ): (
+		// <p>Sorry, we could not load data from Last.fm</p>
 	);
 };
