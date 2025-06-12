525e237779051c47e7159a207b08a31a6078ada8
Simon Hudson <hellosimonhudson@gmail.com>
2025-06-12 09:27:26 +0100

    palette update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import { Lora, Open_Sans } from 'next/font/google';
+import { Lora, Poppins } from 'next/font/google';
 import type { Metadata } from 'next';
 import Script from 'next/script';
 import '@/src/theme/global.scss';
@@ -9,10 +9,11 @@ export const metadata: Metadata = {
 	description: 'Simon Hudson is a Full Stack Developer based in Derby, UK.',
 };
 
-const openSans = Open_Sans({
+const poppins = Poppins({
 	subsets: ['latin'],
 	display: 'swap',
 	variable: '--font-sans-serif',
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 const lora = Lora({
@@ -23,7 +24,7 @@ const lora = Lora({
 
 const Layout = async ({ children }: { children: ReactNode }) => {
 	return (
-		<html lang="en" className={`${openSans.variable} ${lora.variable}`}>
+		<html lang="en" className={`${poppins.variable} ${lora.variable}`}>
 			<body>
 				<>
 					{children}
