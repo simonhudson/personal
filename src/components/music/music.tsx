41062d64e9f92c1f615996859aa78757cbad91f7
Simon Hudson <hellosimonhudson@gmail.com>
2025-12-08 09:49:31 +0000

    convert music to server component

--- src/components/music/music.tsx
+++ src/components/music/music.tsx
@@ -1,48 +1,43 @@
-'use client';
-import { useState, useEffect } from 'react';
+import { Suspense } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './music.module.scss';
 import { type MusicResponse } from '@/src/types/music';
 
-export const Music = () => {
-	const [data, setData] = useState<MusicResponse | null>(null);
-	const [isLoading, setIsLoading] = useState<boolean>(true);
+const MusicInfo = async () => {
+	const response = await fetch(`${process.env.NEXT_PUBLIC_APP_DOMAIN}/api/music`, {
+		cache: 'no-store',
+	});
+	const data: MusicResponse = await response.json();
+	return (
+		<>
+			<Image
+				alt={`Now playing "${data.name}" by ${data.artist}`}
+				className={styles.image}
+				height={300}
+				src={data.image}
+				width={300}
+			/>
+			<p className={styles.text}>
+				<span className={styles.heading}>
+					<Link href={data.url}> {data.artist}</Link>
+				</span>
+				<span>
+					<Link href={data.url}>&quot;{data.name}&quot;</Link>{' '}
+				</span>
+				<span>{data.relativeTime}</span>
+			</p>
+		</>
+	);
+};
 
-	useEffect(() => {
-		(async () => {
-			const response = await fetch(`${process.env.NEXT_PUBLIC_APP_DOMAIN}/api/music`);
-			const responseData: MusicResponse = await response.json();
-			setData(responseData);
-			setIsLoading(false);
-		})();
-	}, []);
-
+export const Music = async () => {
 	return (
 		<div className={styles.wrap}>
 			<div className={styles.inner}>
-				{isLoading && <p>Loading&hellip;</p>}
-				{!isLoading && !data && <p>Sorry, we could not load data from Last.fm</p>}
-				{!isLoading && data && (
-					<>
-						<Image
-							alt={`Now playing "${data.name}" by ${data.artist}`}
-							className={styles.image}
-							height={300}
-							src={data.image}
-							width={300}
-						/>
-						<p className={styles.text}>
-							<span className={styles.heading}>
-								<Link href={data.url}> {data.artist}</Link>
-							</span>
-							<span>
-								<Link href={data.url}>&quot;{data.name}&quot;</Link>{' '}
-							</span>
-							<span>{data.relativeTime}</span>
-						</p>
-					</>
-				)}
+				<Suspense fallback={<div>Loading...</div>}>
+					<MusicInfo />
+				</Suspense>
 			</div>
 		</div>
 	);
