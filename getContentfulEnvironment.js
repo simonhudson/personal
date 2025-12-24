a6ec540280782c4007ef7840e720a93ea418ae17
Simon Hudson <hellosimonhudson@gmail.com>
2025-12-24 10:11:22 +0000

    update env imports

--- getContentfulEnvironment.js
+++ getContentfulEnvironment.js
@@ -4,12 +4,12 @@ import contentful from 'contentful-management';
 
 export default async function getContentfulEnvironment() {
 	const contentfulClient = contentful.createClient({
-		accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN ?? '',
+		accessToken: import.meta.env.CONTENTFUL_MANAGEMENT_TOKEN ?? '',
 	});
 
 	const environment = await contentfulClient.environment.get({
-		spaceId: process.env.NEXT_CONTENTFUL_SPACE_ID ?? '',
-		environmentId: process.env.CONTENTFUL_ENVIRONMENT ?? '',
+		spaceId: import.meta.env.NEXT_CONTENTFUL_SPACE_ID ?? '',
+		environmentId: import.meta.env.CONTENTFUL_ENVIRONMENT ?? '',
 	});
 
 	return environment;
