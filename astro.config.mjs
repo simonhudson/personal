02f6fff3bce164a3bf72c065eda308e7950d53cd
Simon Hudson <hellosimonhudson@gmail.com>
2026-02-18 14:02:27 +0000

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: 'standalone'
-  })
+	adapter: node({
+		mode: 'standalone',
+	}),
+	output: 'static',
 });
