e1a7171b814ba45df5daebb4cacefe13bbdb9f41
Simon Hudson <hellosimonhudson@gmail.com>
2026-02-21 20:50:40 +0000

    projects

---
+++ src/projects.ts
@@ -0,0 +1,25 @@
+export const projects = {
+	'british-red-cross': {
+		title: 'Web Platform',
+		client: 'British Red Cross',
+		description: 'A project for the British Red Cross',
+		image: 'https://picsum.photos/seed/british-red-cross/1200/800',
+		link: 'https://www.redcross.org.uk/',
+	},
+	'experian-your-offers': {
+		title: 'Your Offers',
+		client: 'Experian',
+		description:
+			'A single-page web app which provides authenticated users an aggregate of their recent activity on the main Experian Marketplace site. Users are shown product results from their most recent search, plus suggestions of other products which may be of interest to them.',
+		image: 'https://picsum.photos/seed/experian/1200/800',
+		link: 'https://offers.experian.co.uk',
+	},
+	'experian-marketplace': {
+		title: 'Marketplace',
+		client: 'Experian',
+		description:
+			'A large-scale web app which allows users to search for, and compare, credit cards and loans. The front-end communicated with a huge range of RESTful microservices which were used to manage user authentication, profile information, and product results (amongst many other things). The app was covered by extensive unit and automation tests, and hooked into a CI/CD pipeline built on AWS, which allowed us to release multiple times a day.',
+		image: 'https://picsum.photos/seed/marketplace/1200/800',
+		link: 'https://creditmatcher.experian.co.uk',
+	},
+};
