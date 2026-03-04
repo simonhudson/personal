e5b265c1ce05f5194b1eb15a7da8fc7bd7af51c2
Simon Hudson <hellosimonhudson@gmail.com>
2026-03-04 09:39:40 +0000

    ui work

--- src/components/portfolio/data.ts
+++ src/components/portfolio/data.ts
@@ -1,12 +1,13 @@
 import { slugify } from '@/src/utilities/slugify';
 
-export type Project = {
+export type PortfolioItem = {
 	title: string;
 	client: string;
 	description: string;
 	image: string;
 	link: string;
 	slug: string;
+	madeWith: string[];
 };
 
 const projectsBase = [
@@ -16,6 +17,7 @@ const projectsBase = [
 		description: 'A project for the British Red Cross',
 		image: 'https://picsum.photos/seed/british-red-cross/1200/800',
 		link: 'https://www.redcross.org.uk/',
+		madeWith: ['Next.js', 'Node.js', 'PostgreSQL'],
 	},
 	{
 		title: 'Your Offers',
@@ -24,6 +26,7 @@ const projectsBase = [
 			'A single-page web app which provides authenticated users an aggregate of their recent activity on the main Experian Marketplace site. Users are shown product results from their most recent search, plus suggestions of other products which may be of interest to them.',
 		image: 'https://picsum.photos/seed/experian/1200/800',
 		link: 'https://offers.experian.co.uk',
+		madeWith: ['Next.js', 'Node.js', 'PostgreSQL'],
 	},
 	{
 		title: 'Marketplace',
@@ -32,10 +35,11 @@ const projectsBase = [
 			'A large-scale web app which allows users to search for, and compare, credit cards and loans. The front-end communicated with a huge range of RESTful microservices which were used to manage user authentication, profile information, and product results (amongst many other things). The app was covered by extensive unit and automation tests, and hooked into a CI/CD pipeline built on AWS, which allowed us to release multiple times a day.',
 		image: 'https://picsum.photos/seed/marketplace/1200/800',
 		link: 'https://creditmatcher.experian.co.uk',
+		madeWith: ['Next.js', 'Node.js', 'PostgreSQL'],
 	},
 ];
 
-const projects: Project[] = projectsBase.map((project) => {
+const portfolioData: PortfolioItem[] = projectsBase.map((project) => {
 	const slug = `${slugify(project.client)}-${slugify(project.title)}`;
 	return {
 		...project,
@@ -43,4 +47,4 @@ const projects: Project[] = projectsBase.map((project) => {
 	};
 });
 
-export { projects };
+export { portfolioData };
